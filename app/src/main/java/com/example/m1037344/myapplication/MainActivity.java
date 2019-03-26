b4d5ac67aaf16e1c8ed5bd0d21f31a391a9877e4
ag <toarvind.gupta@gmail.com>
2019-03-26 15:34:39 +0530

    sys out added new

--- app/src/main/java/com/example/m1037344/myapplication/MainActivity.java
+++ app/src/main/java/com/example/m1037344/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Test my application  :");
+        System.out.println("Test my application 2 :");
     }
 }
