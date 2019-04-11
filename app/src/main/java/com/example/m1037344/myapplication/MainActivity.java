16e9f629dbca7f1c403cf40aaeab2be8a514b319
ag <toarvind.gupta@gmail.com>
2019-04-11 12:13:58 +0530

    toast added

--- app/src/main/java/com/example/m1037344/myapplication/MainActivity.java
+++ app/src/main/java/com/example/m1037344/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.m1037344.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"apk finally generated",Toast.LENGTH_SHORT).show();
         System.out.println("Test my application  :");
         System.out.println("Test my application 2 :");
         System.out.println("Test my application apk :");
