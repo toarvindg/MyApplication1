013f03e9d9cb5615510833df500429a38569f3b0
ag <toarvind.gupta@gmail.com>
2019-04-16 17:15:31 +0530

    toast added

--- app/src/main/java/com/example/m1037344/myapplication/MainActivity.java
+++ app/src/main/java/com/example/m1037344/myapplication/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.m1037344.myapplication;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
